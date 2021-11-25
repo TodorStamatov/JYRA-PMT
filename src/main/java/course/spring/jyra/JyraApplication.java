66d13517909fe8e886f15744bb08e81f85fc9cc9
Богдан Косев <bogdan_gamer@abv.bg>
2021-11-25 09:00:25 +0200

    Initial commit

---
+++ src/main/java/course/spring/jyra/JyraApplication.java
@@ -0,0 +1,13 @@
+package course.spring.jyra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/course/spring/jyra/JyraApplicationTests.java
@@ -0,0 +1,13 @@
+package course.spring.jyra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
