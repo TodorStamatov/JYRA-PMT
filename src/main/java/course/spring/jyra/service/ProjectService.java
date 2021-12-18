6fe10600e1705bb8d25bb4de5f7e8f4fb4a8ab07
toshko <toshko_stamatov@abv.bg>
2021-12-18 15:59:11 +0200

    service interface

---
+++ src/main/java/course/spring/jyra/service/ProjectService.java
@@ -0,0 +1,16 @@
+package course.spring.jyra.service;
+
+import course.spring.jyra.model.Project;
+
+import java.util.List;
+
+public interface ProjectService {
+    List<Project> findAll();
+    Project findById(long id);
+    Project findByTitle(String title);
+    Project create(Project project);
+    Project deleteById(long id);
+    Project update(Project project);
+    long count();
+
+}
