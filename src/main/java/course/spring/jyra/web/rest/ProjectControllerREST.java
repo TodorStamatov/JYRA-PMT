1ede87a881b377655e31eab6a718f026afa16e34
toshko <toshko_stamatov@abv.bg>
2021-12-23 10:49:31 +0200

    ProjectControllerREST not finished

--- src/main/java/course/spring/jyra/web/rest/ProjectControllerREST.java
+++ src/main/java/course/spring/jyra/web/rest/ProjectControllerREST.java
@@ -4,10 +4,8 @@ import course.spring.jyra.dao.ProjectRepository;
 import course.spring.jyra.exception.EntityNotFoundException;
 import course.spring.jyra.model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +27,12 @@ public class ProjectControllerREST {
     @GetMapping("/{projectId}")
     public Project getProjectById(@PathVariable String projectId) {
         return projectRepository.findById(projectId).orElseThrow(() -> new EntityNotFoundException(String.format("Project with ID=%s not found.", projectId)));
+    }
+
+    @PostMapping
+    public ResponseEntity<Project> addProject(@RequestBody Project project) {
+        Project created = projectRepository.insert(project);
+        return null;
     }
 
 
