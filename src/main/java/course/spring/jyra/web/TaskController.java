307ae7ce6004f2a1855d3294579c504d6ef0f41d
toshko <toshko_stamatov@abv.bg>
2022-02-14 12:52:03 +0200

    imports

--- src/main/java/course/spring/jyra/web/TaskController.java
+++ src/main/java/course/spring/jyra/web/TaskController.java
@@ -4,6 +4,8 @@ import course.spring.jyra.model.*;
 import course.spring.jyra.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
